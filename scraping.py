c056b31b6d9450495e0eb78278b5c4b9d817f8cc
Nithin Pillai <61659869+bipitybop101@users.noreply.github.com>
2024-04-27 17:00:51 -0400

    created the overlaps file

--- scraping.py
+++ scraping.py
@@ -2,29 +2,6 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-class Department: 
-    ## name-> string
-    ## courses -> Course[]
-    def __init__(self, name, courses):
-        self.name = name
-        self.courses = courses
-
-class Course: 
-    ## course_name -> string 
-    ## sections -> Sections[]
-    def __init__(self, course_name, sections):
-        self.course_name = course_name
-        self.sections = sections
-
-class Section: 
-    ## section_num -> num
-    ## professor_name -> string
-    def __init__(self, section_num, professor_name):
-        self.section_num = section_num
-        self.professor_name = professor_name
-
-
-
 def get_departments(): 
     URL = "https://app.testudo.umd.edu/soc/"
     page = requests.get(URL)
